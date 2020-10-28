30e6d1c67e77ffe1f50332ad84c166e97208326e
Sagnik Sarkar <sagnik106@gmail.com>
2020-10-29 03:33:15 +0530

    Added multiple pages and routes

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from flask import Flask, request, render_template, redirect, session
 from firebase import firebase
 
@@ -13,7 +15,7 @@ def login():
     except:
         pass
     if request.method == "POST":
-        us=fb.get('/',request.form['uname'])
+        us=fb.get('/user/',request.form['uname'])
         if us['pwd']==request.form['pwd']:
             us.pop('pwd')
             for key in us.keys():
@@ -30,7 +32,12 @@ def signup():
         pass
     if request.method == "POST":
         payl=request.form
-        fb.put('/','%s'%(payl['uname']), data=payl)
+        fb.put('/user/','%s'%(payl['uname']), data=payl)
+        us=fb.get('/user/',payl['uname'])
+        us.pop('pwd')
+        for key in us.keys():
+            session[key]=us[key]
+        return redirect('/')
     return render_template('signup.html')
 
 @app.route('/profile')
@@ -45,7 +52,7 @@ def landing():
 def defaul():
     try:
         _=session['uname']
-        return redirect('profile')
+        return redirect('/profile')
     except:
         pass
     return render_template('index.html')
@@ -59,5 +66,30 @@ def logou():
     session.clear()
     return redirect('/')
 
+@app.route('/gallery', methods=["POST", "GET"])
+def gallery():
+    try:
+        _=session["uname"]
+        return render_template("gallery.html")
+    except:
+        return redirect("/")
+
+@app.route('/dish/<name>', methods=["POST", "GET"])
+def dish(name):
+    try:
+        _=session["uname"]
+    except:
+        return redirect("/")
+    if request.method=="POST":
+        payl = dict(request.form)
+        print(type(payl))
+        payl["fname"]=session["fname"]
+        payl["lname"]=session["lname"]
+        payl["uname"]=session["uname"]
+        fb.put('/review/%s/'%(name),'%s'%(payl['subject']), data=payl)
+    rev=fb.get('/review/', name)
+    reviews=list(rev.values())
+    return render_template("dish.html", urname=name, reviews=reviews)
+
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port="80", debug=True)
+    app.run(debug=True)
